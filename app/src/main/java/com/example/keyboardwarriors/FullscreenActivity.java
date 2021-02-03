859e1c37a4275b7e66305ddebaafb02c9a5fbe4e
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-02-03 13:49:15 -0500

    Update FullscreenActivity.java

--- app/src/main/java/com/example/keyboardwarriors/FullscreenActivity.java
+++ app/src/main/java/com/example/keyboardwarriors/FullscreenActivity.java
@@ -39,7 +39,7 @@ public class FullscreenActivity extends AppCompatActivity {
         @Override
         public void run() {
             // Delayed removal of status and navigation bar
-
+// YO
             // Note that some of these constants are new as of API 16 (Jelly Bean)
             // and API 19 (KitKat). It is safe to use them, as they are inlined
             // at compile-time and do nothing on earlier devices.
