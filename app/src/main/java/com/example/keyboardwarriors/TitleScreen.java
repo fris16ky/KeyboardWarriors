a0a59757f31eaa57ae235a8c2cd1c9f249d76cf3
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-02-07 18:32:21 -0500

    Color Update

--- app/src/main/java/com/example/keyboardwarriors/TitleScreen.java
+++ app/src/main/java/com/example/keyboardwarriors/TitleScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class TitleScreen extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class TitleScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_title_screen);
 
         Button button2 = (Button) findViewById(R.id.button2);
