0bbefc44d0d7d0774039c6219fe2cca61b5f2f67
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-02-06 21:13:30 -0500

    Updated Exit Button

--- app/src/main/java/com/example/keyboardwarriors/TitleScreen.java
+++ app/src/main/java/com/example/keyboardwarriors/TitleScreen.java
@@ -5,17 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class TitleScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
     }
+
     public void ToStartGame(View view){
         Intent game = new Intent(this, GameScreen.class);
         startActivity(game);
 
     }
+
 }
