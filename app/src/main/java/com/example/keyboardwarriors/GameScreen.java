e7394e82bc4e7f8a7e14fd21f935269af038d818
Kitus <kevinkarafili01@gmail.com>
2021-02-07 16:55:42 -0500

    Back button made

--- app/src/main/java/com/example/keyboardwarriors/GameScreen.java
+++ app/src/main/java/com/example/keyboardwarriors/GameScreen.java
@@ -14,20 +14,14 @@ public class GameScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game_screen);
 
-        Button button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-                System.exit(1);
-            }
-        });
 
     }
+    public void ToTitleScreen(View view){
+        Intent game = new Intent(this, TitleScreen.class);
+        startActivity(game);
 
-
-
+    }
 }
