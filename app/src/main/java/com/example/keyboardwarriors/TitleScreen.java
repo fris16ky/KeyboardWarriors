1549b0e27307fce584dfcac6fab8589431418d4e
Kitus <kevinkarafili01@gmail.com>
2021-02-07 19:51:23 -0500

    exit button gone

--- app/src/main/java/com/example/keyboardwarriors/TitleScreen.java
+++ app/src/main/java/com/example/keyboardwarriors/TitleScreen.java
@@ -17,14 +17,7 @@ public class TitleScreen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_title_screen);
 
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-                System.exit(0);
-            }
-        });
+
 
 
     }
