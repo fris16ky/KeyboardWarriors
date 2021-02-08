e2c8b0bc4bedebe7c1a45952b011b47cafd573f1
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-02-08 16:06:19 -0500

    Added a keyboard that automatically pops up

--- app/src/main/java/com/example/keyboardwarriors/GameScreen.java
+++ app/src/main/java/com/example/keyboardwarriors/GameScreen.java
@@ -1,22 +1,40 @@
 package com.example.keyboardwarriors;
 
+import androidx.annotation.MainThread;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.system.Os;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.security.AllPermission;
 
 public class GameScreen extends AppCompatActivity {
 
+    Button mButton;
+    EditText mEdit;
+    TextView mText;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game_screen);
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
     }
     public void ToTitleScreen(View view){
@@ -24,4 +42,5 @@ public class GameScreen extends AppCompatActivity {
         startActivity(game);
 
     }
+    
 }
