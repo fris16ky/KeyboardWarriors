ae67918c6776a42e1ab7fe2308a1f2cf20e2c172
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-02-09 20:40:31 -0500

    major updates

--- app/src/main/java/com/example/keyboardwarriors/TitleScreen.java
+++ app/src/main/java/com/example/keyboardwarriors/TitleScreen.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class TitleScreen extends AppCompatActivity {
 
+    ImageButton start;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +20,15 @@ public class TitleScreen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_title_screen);
 
+        start = (ImageButton) findViewById(R.id.start);
 
-
-
-    }
-
-    public void ToStartGame(View view){
-        Intent game = new Intent(this, GameScreen.class);
-        startActivity(game);
-
-    }
-    public void ToOptionScreen(View view){
-        Intent game = new Intent(this, OptionScreen.class);
-        startActivity(game);
-
+        start.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public  void onClick(View view){
+                startActivity(new Intent(getApplicationContext(), GameScreen.class));
+                finish();
+            }
+        });
     }
 
 }
