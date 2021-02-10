49b79633764742a7e4b259c3b1a0c9773d3bf83c
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-02-10 00:20:38 -0500

    major updates

--- app/src/main/java/com/example/keyboardwarriors/OptionScreen.java
+++ app/src/main/java/com/example/keyboardwarriors/OptionScreen.java
@@ -6,20 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class OptionScreen extends AppCompatActivity {
 
+    ImageButton back2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_option_screen);
+
+        back2 = (ImageButton) findViewById(R.id.back2);
+        back2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+                startActivity(new Intent(getApplicationContext(), TitleScreen.class));
+                finish();
+            }
+        });
+
+
+
+
+
     }
 
-
-    public void ToTitleScreen(View view){
-        Intent game = new Intent(this, TitleScreen.class);
-        startActivity(game);
-
-    }
 }
---
+++ app/src/main/java/com/example/keyboardwarriors/TutorialScreen.java
@@ -0,0 +1,19 @@
+package com.example.keyboardwarriors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TutorialScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial_screen);
+
+        final String[] MyWords = {"Final", "Way", "Exit"};
+
+
+
+    }
+}
