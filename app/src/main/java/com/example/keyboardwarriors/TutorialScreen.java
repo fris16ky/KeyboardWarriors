52fd01af8c67607912c94ca6a0cc1a9fd2e5390e
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-02-11 22:03:53 -0500

    major updates

--- app/src/main/java/com/example/keyboardwarriors/TutorialScreen.java
+++ app/src/main/java/com/example/keyboardwarriors/TutorialScreen.java
@@ -3,26 +3,116 @@ package com.example.keyboardwarriors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.Array;
-import java.text.BreakIterator;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class TutorialScreen extends AppCompatActivity {
 
-    TextView text;
-    Random randomGenerator;
+
+    TextView d_text;
+    EditText user_input;
+    ImageButton s_check, n_check;
+
+    Random r;
+
+    String [] words_list = {"all\n" +
+                            "am\n" +
+                            "and\n" +
+                            "at\n" +
+                            "ball\n" +
+                            "be\n" +
+                            "bed\n" +
+                            "big\n" +
+                            "book\n" +
+                            "box\n" +
+                            "boy\n" +
+                            "but\n" +
+                            "came\n" +
+                            "can\n" +
+                            "car\n" +
+                            "cat\n" +
+                            "come\n" +
+                            "cow\n" +
+                            "dad\n" +
+                            "day\n" +
+                            "did\n" +
+                            "do\n" +
+                            "dog\n" +
+                            "fat\n" +
+                            "for\n" +
+                            "fun\n" +
+                            "get\n" +
+                            "go\n" +
+                            "good\n" +
+                            "got\n" +
+                            "had\n" +
+                            "hat\n" +
+                            "he\n" +
+                            "hen\n" +
+                            "here\n" +
+                            "him\n" +
+                            "his\n" +
+                            "home\n" +
+                            "hot\n" +
+                            "I\n" +
+                            "if\n" +
+                            "in\n" +
+                            "into\n" +
+                            "is\n" +
+                            "it\n" +
+                            "its\n" +
+                            "let\n" +
+                            "like\n" +
+                            "look\n" +
+                            "man\n" +
+                            "may\n" +
+                            "me\n" +
+                            "mom\n" +
+                            "my\n" +
+                            "no\n" +
+                            "not\n" +
+                            "of\n" +
+                            "oh\n" +
+                            "old\n" +
+                            "on\n" +
+                            "one\n" +
+                            "out\n" +
+                            "pan\n" +
+                            "pet\n" +
+                            "pig\n" +
+                            "play\n" +
+                            "ran\n" +
+                            "rat\n" +
+                            "red\n" +
+                            "ride\n" +
+                            "run\n" +
+                            "sat\n" +
+                            "see\n" +
+                            "she\n" +
+                            "sit\n" +
+                            "six\n" +
+                            "so\n" +
+                            "stop\n" +
+                            "sun\n" +
+                            "ten\n" +
+                            "the\n" +
+                            "this\n" +
+                            "to\n" +
+                            "top\n" +
+                            "toy\n" +
+                            "two\n" +
+                            "up\n" +
+                            "us\n" +
+                            "was\n" +
+                            "we\n" +
+                            "will\n" +
+                            "yes\n" +
+                            "you"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +120,32 @@ public class TutorialScreen extends AppCompatActivity {
         setContentView(R.layout.activity_tutorial_screen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        d_text = (TextView) findViewById(R.id.display_text);
+
+        user_input = (EditText) findViewById(R.id.input_text);
+
+        s_check = (ImageButton) findViewById(R.id.submit);
+        n_check = (ImageButton) findViewById(R.id.new_word);
+
+       /* s_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        n_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        */
+
+
+
+
     }
-
-
-
 }
 
 
