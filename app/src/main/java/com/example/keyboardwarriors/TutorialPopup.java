f77df5fa7b626d3fd8698db6e73fd73635045439
Keiji Roemmich <keiji0627@live.com>
2021-03-18 22:58:51 -0400

    Edited TutorialScript button, added close button to pop up

--- app/src/main/java/com/example/keyboardwarriors/TutorialPopup.java
+++ app/src/main/java/com/example/keyboardwarriors/TutorialPopup.java
@@ -5,13 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class TutorialPopup extends Activity {
+
+    Button btn_close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial_popup);
+
+       btn_close  = (Button) findViewById(R.id.btn_close);
+       btn_close.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               finish();
+           }
+       });
+
+
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -20,6 +36,13 @@ public class TutorialPopup extends Activity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*.8),(int)(height*.7));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
 
 
     }
