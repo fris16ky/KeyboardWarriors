4052cf0a7da19e2d09e1a5efb36fdaffe988bd03
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-04-06 19:50:20 -0400

    added MediumMode

--- app/src/main/java/com/example/keyboardwarriors/HardMode.java
+++ app/src/main/java/com/example/keyboardwarriors/HardMode.java
@@ -1,4 +1,13 @@
 package com.example.keyboardwarriors;
 
-public class HardMode {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HardMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hard_mode);
+    }
 }
