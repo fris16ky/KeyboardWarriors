2f53236152e12937136d6bf1aa49ae12efbbd5bc
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-04-06 19:54:54 -0400

    added boss class

---
+++ app/src/main/java/com/example/keyboardwarriors/BossScreen.java
@@ -0,0 +1,14 @@
+package com.example.keyboardwarriors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BossScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss_screen);
+    }
+}
