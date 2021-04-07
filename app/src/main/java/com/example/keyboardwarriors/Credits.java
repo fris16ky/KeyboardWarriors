fe6a2a9043521367c8ccab8579c499496696d4b7
Kitus <kevinkarafili01@gmail.com>
2021-04-07 19:11:05 -0400

    added our names

--- app/src/main/java/com/example/keyboardwarriors/Credits.java
+++ app/src/main/java/com/example/keyboardwarriors/Credits.java
@@ -2,13 +2,20 @@ package com.example.keyboardwarriors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Credits extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_credits);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+
         setContentView(R.layout.activity_credits);
     }
 }
