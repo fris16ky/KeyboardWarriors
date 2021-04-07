73108c349a90185876908937048d24b6c9f4c785
sneh734 <66852907+sneh734@users.noreply.github.com>
2021-04-06 20:01:19 -0400

    added boss class

--- app/src/main/java/com/example/keyboardwarriors/EasyModeBoss.java
+++ app/src/main/java/com/example/keyboardwarriors/EasyModeBoss.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BossScreen extends AppCompatActivity {
+public class EasyModeBoss extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_boss_screen);
+        setContentView(R.layout.activity_easy_mode_boss);
     }
 }
