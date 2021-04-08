9bd46295df90a144c027cef539f32c96fb22c7a8
Keiji Roemmich <keiji0627@live.com>
2021-04-08 18:43:36 -0400

    Added boss transition hard

---
+++ app/src/main/java/com/example/keyboardwarriors/BossTransitionHard.java
@@ -0,0 +1,29 @@
+package com.example.keyboardwarriors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class BossTransitionHard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss_transition_hard);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    //startActivity(new Intent(getApplicationContext(), HardModeBoss.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
