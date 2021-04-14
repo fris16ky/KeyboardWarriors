fbc0b2fc1481bad39f6b42fcf8af545ed6eea852
Kitus <kevinkarafili01@gmail.com>
2021-04-13 21:06:15 -0400

    added back button to credits

--- app/src/main/java/com/example/keyboardwarriors/Credits.java
+++ app/src/main/java/com/example/keyboardwarriors/Credits.java
@@ -2,11 +2,15 @@ package com.example.keyboardwarriors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class Credits extends AppCompatActivity {
+    ImageButton back3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,13 @@ public class Credits extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
 
         setContentView(R.layout.activity_credits);
+        back3 = (ImageButton) findViewById(R.id.back3);
+        back3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), OptionScreen.class));
+                finish();
+            }
+        });
     }
 }
